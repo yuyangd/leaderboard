3cd9d6ba306e059535b6ecc7e938396642963b53
Du Yuyang <du.yuyang@rea-group.com>
2019-05-11 21:07:34 +1000

    code migration

--- src/App.js
+++ src/App.js
@@ -2,25 +2,43 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const Racer = ({color, position, name, time}) => {
+  return (
+    <tr className={color}>
+      <th scope="row">{position}</th>
+      <td>{name}</td>
+      <td className="time">{time}</td>
+    </tr>
+  )
+}
+
+const App = ({racers}) => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <table className="table table-sm table-dark text">
+        <thead>
+          <tr>
+            <th scope="col">Position</th>
+            <th scope="col">Racer</th>
+            <th scope="col">Time</th>
+          </tr>
+        </thead>
+        <tbody>
+          <Racer color="bg-gold" position={1} name={racers[0].name} time={racers[0].time}/>
+          <Racer color="bg-silver" position={2} name={racers[1].name} time={racers[1].time}/>
+          <Racer color="bg-copper" position={3} name={racers[2].name} time={racers[2].time}/>
+        </tbody>
+
+        <tbody>
+          <Racer position={4} name={racers[3].name} time={racers[3].time}/>
+          <Racer position={5} name={racers[4].name} time={racers[4].time}/>
+        </tbody>
+      </table>
+    </header>
+  </div>
+  )
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -4,7 +4,15 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+let racerList = [
+  {"name": "Mark", "time": "00:12.020"},
+  {"name": "Jacob", "time": "00:12.020"},
+  {"name": "Larry", "time": "00:12.020"},
+  {"name": "Ayer", "time": "00:12.020"},
+  {"name": "Jon", "time": "00:12.020"},
+]
+
+ReactDOM.render(<App racers={racerList}/>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.

4fca15d655b6c975b64f2a07d2aca2506a49caac
Du Yuyang <du.yuyang@rea-group.com>
2019-05-12 12:11:00 +1000

    update backend to serve color and position

--- src/App.js
+++ src/App.js
@@ -13,15 +13,6 @@ const Racer = ({color, position, name, time}) => {
 }
 
 class App extends React.Component {
-  static defaultProps = {
-    racers: [
-      {"name": "Mark", "time": "00:12.020"},
-      {"name": "Jacob", "time": "00:12.020"},
-      {"name": "Larry", "time": "00:12.020"},
-      {"name": "Ayer", "time": "00:12.020"},
-      {"name": "Jon", "time": "00:12.020"},
-    ]
-  }
   state = {
     racers: [],
   }
@@ -45,52 +36,33 @@ class App extends React.Component {
 
   render(){
     return (
-      <div>
+      <div className="App">
         {this.state.loading
           ? "loading..."
-          : <div>
+          : <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <table className="table table-sm table-dark text">
+            <thead>
+              <tr>
+                <th scope="col">Position</th>
+                <th scope="col">Racer</th>
+                <th scope="col">Time</th>
+              </tr>
+            </thead>
+            <tbody>
             
             {this.state.racers.map(racer => {
               return (
-                <div>
-                  <h4>{racer.name}</h4>
-                  <p>{racer.time}</p>
-                </div>
+                <Racer key={racer.position} color={racer.color} position={racer.position} name={racer.name} time={racer.time}/>
               )
             } )}
-          </div>
+              </tbody>
+            </table>
+          </header>
         }
       </div>
     )
   }
 }
-// const App = ({racers}) => {
-//   return (
-//     <div className="App">
-//     <header className="App-header">
-//       <img src={logo} className="App-logo" alt="logo" />
-//       <table className="table table-sm table-dark text">
-//         <thead>
-//           <tr>
-//             <th scope="col">Position</th>
-//             <th scope="col">Racer</th>
-//             <th scope="col">Time</th>
-//           </tr>
-//         </thead>
-//         <tbody>
-//           <Racer color="bg-gold" position={1} name={racers[0].name} time={racers[0].time}/>
-//           <Racer color="bg-silver" position={2} name={racers[1].name} time={racers[1].time}/>
-//           <Racer color="bg-copper" position={3} name={racers[2].name} time={racers[2].time}/>
-//         </tbody>
-
-//         <tbody>
-//           <Racer position={4} name={racers[3].name} time={racers[3].time}/>
-//           <Racer position={5} name={racers[4].name} time={racers[4].time}/>
-//         </tbody>
-//       </table>
-//     </header>
-//   </div>
-//   )
-// }
 
 export default App;

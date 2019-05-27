ea76908fe7d6ebbd4cc9bbbc9312301404401b44
Du Yuyang <du.yuyang@rea-group.com>
2019-05-27 17:22:55 +1000

    the flask api

--- backend/main.py
+++ backend/main.py
@@ -1,13 +1,20 @@
 import flask
 from racersheet import RacerSheet
+from cachetools import cached, TTLCache
 
 app = flask.Flask(__name__)
+cache = TTLCache(maxsize=100, ttl=60)
 
-@app.route('/')
-def racer_data():
+@cached(cache)
+def read_cache():
   rs = RacerSheet()
   rs.sort_sheet()
   resp = flask.Response(rs.fetch_sheet())
+  return resp
+
+@app.route('/')
+def racer_data():
+  resp = read_cache()
   resp.headers['Access-Control-Allow-Origin'] = '*'
   resp.headers['Access-Control-Request-Method'] = '*'
   return resp
